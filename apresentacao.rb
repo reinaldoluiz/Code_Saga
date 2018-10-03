38643e63e328bcdd0beaf1e3eb86ff7e3b58de60
reinaldoluiz <reinaldolluiz@gmail.com>
2018-10-03 11:58:51 -0300

    Ex 02 - Ruby

--- apresentacao.rb
+++ apresentacao.rb
@@ -5,3 +5,13 @@ print "Tenho 33 anos\n"
 print "Moro em São Paulo\n"
 print ""
 print "Meu nome é Reinaldo\n"
+
+#02
+
+puts "Digite seu nome:"
+name = gets
+puts "Digite sua idade:"
+age = gets
+
+puts "Seu nome é #{name}"
+puts "Seu nome é #{age}"
