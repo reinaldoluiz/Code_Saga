2d5217709cf18b80f646fd4852a2ec00416072c5
reinaldoluiz <reinaldolluiz@gmail.com>
2018-10-03 12:04:08 -0300

    Ex 03 - Ruby

--- apresentacao.rb
+++ apresentacao.rb
@@ -1,17 +1,28 @@
 #01
 
-print "Meu nome é Reinaldo\n"
-print "Tenho 33 anos\n"
-print "Moro em São Paulo\n"
-print ""
-print "Meu nome é Reinaldo\n"
+#print "Meu nome é Reinaldo\n"
+#print "Tenho 33 anos\n"
+#print "Moro em São Paulo\n"
+#print ""
+#print "Meu nome é Reinaldo\n"
 
 #02
 
-puts "Digite seu nome:"
-name = gets
-puts "Digite sua idade:"
-age = gets
+#puts "Digite seu nome:"
+#name = gets
+#puts "Digite sua idade:"
+#age = gets
 
-puts "Seu nome é #{name}"
-puts "Seu nome é #{age}"
+#puts "Seu nome é #{name}"
+#puts "Seu nome é #{age}"
+
+#03
+
+puts "Digite sua idade:"
+age = gets.to_i
+
+if age < 18
+  puts 'Você não está apto a dirigir'
+else
+  puts 'Você está apto a dirigir'
+end
