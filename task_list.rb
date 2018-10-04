950d14dfc5eddeefa9dd72f6b5122035f1a2de20
reinaldoluiz <reinaldolluiz@gmail.com>
2018-10-04 11:56:15 -0300

    Answered Estruturas de repetição

--- task_list.rb
+++ task_list.rb
@@ -1,18 +1,40 @@
 puts "Bem-vindo ao Task List! Escolha uma opção no menu: \n"
 puts '[1] Inserir uma tarefa'
 puts '[2] Ver todas as tarefas'
+puts '[3] Clear'
+puts '[4] Sair'
 puts
 print 'Opção escolhida: '
 
 opcao = gets.to_i
 tarefa = ""
+tarefas = []
 
-if opcao == 1
-  print 'Digite sua tarefa: '
-  tarefa = gets
+while opcao != 4 do
+  if opcao == 1
+    print 'Digite sua tarefa: '
+    tarefa = gets
+    tarefas << tarefa
+    puts
+    puts 'Tarefa cadastrada: ' + tarefa
+  elsif opcao == 2
+    puts
+    puts tarefas
+  elsif opcao == 3
+    system('clear')
+  else
+    puts
+    puts 'Opção inválida'
+  end
   puts
-  puts 'Tarefa cadastrada: ' + tarefa
-else
   puts
-  puts tarefa
+  puts
+  puts "Bem-vindo ao Task List! Escolha uma opção no menu: \n"
+  puts '[1] Inserir uma tarefa'
+  puts '[2] Ver todas as tarefas'
+  puts '[3] Clear'
+  puts '[4] Sair'
+  puts
+  print 'Opção escolhida: '
+  opcao = gets.to_i
 end
